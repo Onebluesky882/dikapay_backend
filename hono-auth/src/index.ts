726cffc326270c33e90d45c7fc03994c69a985e1
Onebluesky882 <onebluesky882@outlook.com>
2026-02-12 16:14:21 +0700

    build : hono better auth

--- hono-auth/src/index.ts
+++ hono-auth/src/index.ts
@@ -1,9 +1,40 @@
-import { Hono } from 'hono'
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { db } from "./db";
+import { schema } from "./db/schema";
+const app = new Hono();
 
-const app = new Hono()
+app.use(
+  "/api/*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  trustedOrigins: ["localhost://3000", "dikapayapp://"],
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {},
+});
 
-export default app
+app.all("/api/auth/*", async (c) => {
+  return auth.handler(c.req.raw);
+});
+
+const port = Number(process.env.PORT ?? 3000);
+
+export default {
+  port,
+  fetch: app.fetch,
+};
